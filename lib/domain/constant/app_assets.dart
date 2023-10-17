3f7df128d005720e3016d2d4dc3ad87025293fa4
yudhistira.yoga <yudhistiray198@gmail.com>
2023-10-17 14:37:11 +0700

    chore: remove unused assets

--- lib/domain/constant/app_assets.dart
+++ lib/domain/constant/app_assets.dart
@@ -8,64 +8,10 @@ class AppAssets {
   /// Images Section
   static String get imgOnBoarding => '$_imgPath/images_onboarding.png';
 
-  static String get imgCalendar => '$_imgPath/images_calendar.png';
-
-  static String get imgVaccine => '$_imgPath/images_vaccine.png';
-
-  static String get imgTrack => '$_imgPath/images_track.png';
-
-  static String get imgProduct => '$_imgPath/images_product.png';
-
-  static String get imgService => '$_imgPath/images_dummy_service.png';
-
-  static String get imgService2 => '$_imgPath/images_dummy_2.png';
-
-  static String get imgParticle => '$_imgPath/images_item_bottom_1.png';
-
-  static String get imgAccount => '$_imgPath/images_dummy_account.png';
-
-  static String get imgBg2 => '$_imgPath/images_particle_2.png';
-
-  static String get imgBg3 => '$_imgPath/images-particle_3.png';
-
   /// Icons Section
   static String get icPassword => "$_icPath/ic_password.svg";
 
   static String get icArrowRight => "$_icPath/ic_arrow_right.svg";
 
-  static String get icDisk => "$_icPath/ic_disk.svg";
-
-  static String get icInfo => "$_icPath/ic_info.svg";
-
-  static String get icAccount => "$_icPath/ic_account.svg";
-
-  static String get icMarker => "$_icPath/ic_marker.svg";
-
-  static String get icHospital => "$_icPath/ic_hospital.svg";
-
-  static String get icLocation => "$_icPath/ic_location.svg";
-
-  static String get icStar => "$_icPath/ic_star.svg";
-
-  static String get icFilter => "$_icPath/ic_filter.svg";
-
-  static String get icCart => "$_icPath/ic_cart.svg";
-
-  static String get icNotification => "$_icPath/ic_notification.svg";
-
-  static String get icFb => "$_icPath/ic_fb.svg";
-
-  static String get icIg => "$_icPath/ic_ig.svg";
-
-  static String get icX => "$_icPath/ic_x.svg";
-
   static String get icCopyright => "$_icPath/ic_copyright.svg";
-
-  static String get icMenu => "$_icPath/ic_menu.svg";
-
-  static String get icParticle => "$_icPath/ic_particle.svg";
-
-  static String get icClose => "$_icPath/ic_close.svg";
-
-  static String get icIndicator => "$_icPath/ic_indicator.svg";
 }
